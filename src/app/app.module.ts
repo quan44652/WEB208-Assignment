86aebb1d819b5e145fd5809e14dabf9d64e8a120
quan44652 <quan44652@gmail.com>
2023-05-15 09:38:40 +0700

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { IndexComponent } from './admin/produtcs/index/index.component';
+import { AddComponent } from './admin/produtcs/add/add.component';
+import { UpdateComponent } from './admin/produtcs/update/update.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoryComponent } from './components/category/category.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    IndexComponent,
+    AddComponent,
+    UpdateComponent,
+    HeaderComponent,
+    CategoryComponent,
+    SliderComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
